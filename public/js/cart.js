c02489645571e4c866fe5d93bc9e6b80736d6238
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-04 21:43:23 -0500

    new pictures

--- public/js/cart.js
+++ public/js/cart.js
@@ -21,3 +21,30 @@ const addToCart = async (event) => {
 document.querySelectorAll('.addToCartButton').forEach(button => {
     button.addEventListener('click', addToCart);
 });
+
+const handleRemoveFromCart = async (event) => {
+    event.preventDefault();
+
+    const productId = event.target.getAttribute('data-product-id');
+
+    const sessionResponse = await fetch('/api/users/session', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const response = await fetch(`/api/users/${userId}/cart/removeItem/${productId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        alert('Removed from cart!');
+        document.location.reload();
+    } else {
+        alert('Failed to remove from cart');
+    }
+}
--- seeds/product.js
+++ seeds/product.js
@@ -3,47 +3,52 @@ const Product = require('../models/Product');
 
 const productData = [
     {
-        product_name: 'Plain Ole Chocolate Chip',
+        product_name: 'Chocolate Chip',
         price: 2.00,
         stock: 10,
         featured: true,
         filename: '01-chocolate-chip.jpg',
-        // category_id: 1,
+        
     },
     {
         product_name: 'Peanut Butter',
         price: 2.50,
         stock: 0,
+        featured: false,
         filename: '02-peanut-butter.jpg',
-        // category_id: 1,
+        
     },
     {
         product_name: 'Oatmeal Raisin',
         price: 2.50,
         stock: 10,
+        featured: true,
         filename: '03-oatmeal-raisin.jpg',
-        // category_id: 1,
+        
     },
     {
         product_name: 'White Chocolate Macadamia',
         price: 2.50,
         stock: 10,
+        featured: false,
         filename: '04-white-chocolate-macadamia.jpg',
-        // category_id: 1,
+        
     },
     {
         product_name: 'Sugar',
         price: 2.00,
         stock: 10,
+        featured: true,
         filename: '05-sugar.jpg',
-        // category_id: 1,
+        
     },
     {
         product_name: 'Snickerdoodle',
         price: 2.50,
         stock: 10,
+        featured: true,
         filename: '06-snickerdoodle.jpg',
-        // category_id: 1,
+        
     },
     
 ];
