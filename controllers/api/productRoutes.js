daa1390deef3fd3d96e011d5b31754c1696208f7
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-06 14:33:22 -0500

    added products get route for id specific param

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -12,4 +12,18 @@ router.get('/', async (req, res) => {
 	}
 });
 
+// The `/api/products/:id` endpoint that returns a single product by id
+router.get('/:id', async (req, res) => {
+	try {
+		const productData = await Product.findByPk(req.params.id);
+		if (!productData) {
+			res.status(404).json({ message: 'No product found with that id!' });
+			return;
+		}
+		res.status(200).json(productData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
